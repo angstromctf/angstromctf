595e4f8a654c538bed0c8df8a52e7dd36eb077af
Noah Singer <singerng@gmail.com>
2016-11-28 21:08:07 -0500

    switched to monospace; dropdown for archive

--- app/about.component.ts
+++ app/about.component.ts
@@ -9,7 +9,7 @@ import 'rxjs/add/operator/toPromise';
 })
 export class AboutComponent implements OnInit {
     people: any = [
-        {name: 'Noah Singer', role: 'Coordinator, lead platform developer, learning materials', year: 'Junior'},
+        {name: 'Noah Singer', role: 'Director, lead platform developer, learning materials', year: 'Junior'},
         {name: 'Artemis Tosini', role: 'Deployment and infrastructure', year: 'Junior', site: 'https://artemis.re/'},
         {name: 'Andrew Komo', role: 'Crypto problems', year: 'Junior'},
         {name: 'George Klees', role: 'Binary and reverse engineering problems', year: 'Junior'},
