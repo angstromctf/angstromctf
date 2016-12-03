fe2ab3c01735f83cb31d619cab6e7f32b86a891d
Noah Singer <singerng@gmail.com>
2016-12-02 22:13:53 -0500

    added a basic countdown :D

--- app/index.component.ts
+++ app/index.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
+
 @Component({
     moduleId: module.id,
     selector: 'angstrom-index',
     templateUrl: 'index.component.html',
 })
 export class IndexComponent implements OnInit {
-    teams: any;
+    UNITS: string[] = ["days", "hours", "minutes", "seconds"];
+    DURATIONS: number[] = [100, 24, 60, 60];
+    time: number[] = [0, 0, 0, 0];
 
     constructor(private title: Title) { }
 
     ngOnInit(): void {
         this.title.setTitle("ångstromCTF");
+
+        var secs = Math.floor((Date.parse("Mar 11, 2017") - Date.now()) / 1000);
+
+        for (var i = 3; i >= 0; i--) {
+            this.time[i] = secs % this.DURATIONS[i];
+            secs = Math.floor(secs / this.DURATIONS[i]);
+        }
+
+        setInterval(() => {
+            for (var i = 3; i >= 0; i--) {
+                this.time[i] -= 1;
+
+                if (this.time[i] < 0) this.time[i] = this.DURATIONS[i] - 1;
+                else break;
+            }
+        }, 1000);
     }
 }
