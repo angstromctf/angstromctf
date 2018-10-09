d48f4683b839bd2bbdf800731084a05b87d87bae
Kevin Higgs <github@kevinhiggs.com>
2018-10-09 19:58:22 -0400

    update people

--- src/app/content/about.component.ts
+++ src/app/content/about.component.ts
@@ -13,15 +13,9 @@ import 'rxjs/add/operator/toPromise';
 })
 export class AboutComponent implements OnInit {
   people: any = [
-    {name: 'Noah Singer', role: 'Director, technical lead, learning materials', year: 'Senior', site: 'https://singerng.github.io/'},
-    {name: 'Artemis Tosini', role: 'Deployment and infrastructure', year: 'Senior', site: 'https://artemis.re/'},
-    {name: 'Andrew Komo', role: 'Problems lead', year: 'Senior'},
-    {name: 'George Klees', role: 'Binary and reverse engineering problems', year: 'Senior'},
-    {name: 'Chris Wang', role: 'Graphic design', year: 'Senior'},
-    {name: 'Noah Kim', role: 'Platform developer', year: 'Senior', site: 'https://noahbkim.com/'},
-    {name: 'Ian Rackow', role: 'Outreach, sponsors, web problems', year: 'Junior'},
-    {name: 'William Wang', role: 'Problems', year: 'Junior', site: 'https://defund.io'},
-    {name: 'Kevin Higgs', role: 'Problems, platform developer', year: 'Freshman', site: 'https://kmh.zone'}
+    {name: 'Ian Rackow', role: 'Problems and outreach', year: 'Senior'},
+    {name: 'William Wang', role: 'Problems, platform developer', year: 'Senior', site: 'https://defund.io'},
+    {name: 'Kevin Higgs', role: 'Problems, platform developer', year: 'Sophomore', site: 'https://kmh.zone'}
   ];
 
   constructor(private titleService: Title) { }
