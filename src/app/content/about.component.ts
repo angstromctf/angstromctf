fbb99ffdf113ccc93661767fcc86d05cff570483
Ian Rackow <ian.rackow@gmail.com>
2018-10-09 20:35:10 -0400

    Change to about page staff+ordering

--- src/app/content/about.component.ts
+++ src/app/content/about.component.ts
@@ -15,7 +15,8 @@ export class AboutComponent implements OnInit {
   people: any = [
     {name: 'Ian Rackow', role: 'Project lead, problem developer', year: 'Senior'},
     {name: 'William Wang', role: 'Project lead, problem developer', year: 'Senior', site: 'https://defund.io'},
-    {name: 'Kevin Higgs', role: 'Platform lead, problem developer', year: 'Sophomore', site: 'https://kmh.zone'}
+    {name: 'Kevin Higgs', role: 'Platform lead, problem developer', year: 'Sophomore', site: 'https://kmh.zone'},
+    {name: 'Clarence Lam', role: 'Problem developer', year: 'Freshman'}
   ];
 
   constructor(private titleService: Title) { }
@@ -25,9 +26,9 @@ export class AboutComponent implements OnInit {
     this.titleService.setTitle("About | ångstromCTF");
 
     // Adapted from http://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array-in-javascript
-    for (let i = this.people.length; i; i--) {
+    /*for (let i = this.people.length; i; i--) {
       let j = Math.floor(Math.random() * i);
       [this.people[i - 1], this.people[j]] = [this.people[j], this.people[i - 1]];
-    }
+    }*/
   }
 }
